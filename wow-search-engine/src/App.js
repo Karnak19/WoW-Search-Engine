51f65ae6ce400a54d88f395b49f115135b7200b3
Enzo Duhalde <enzo-duhalde@hotmail.fr>
2019-04-04 17:48:45 +0200

    add card character

--- wow-search-engine/src/App.js
+++ wow-search-engine/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Cardcharacter from "./Character";
 class App extends Component {
     render() {
         return (
@@ -10,6 +11,7 @@ class App extends Component {
                         Enter the name of a player below and found out more about his stats and other cool stuff !
                     </p>
                 </header>
+                <Cardcharacter />
             </div>
         );
     }
